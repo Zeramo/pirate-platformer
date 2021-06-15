d5fbd087f9f54f2cce818cb4c23ec750b86e1735
Gabriel Budan <gabriel.budan@campus.lmu.de>
2021-06-15 14:17:44 +0200

    Latest version of old repo

---
+++ Assets/Scripts/background.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background : MonoBehaviour
+{
+    public GameObject go;
+    float distanceFromCamera = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 centerPos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, distanceFromCamera));
+        go.transform.position = centerPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
