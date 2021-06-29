81599a70454ba24b1071aea8cff8c86a861bb4e6
Gabriel Budan <gabriel.budan@campus.lmu.de>
2021-06-29 11:25:39 +0200

    Background always centers

--- Assets/Scripts/background.cs
+++ Assets/Scripts/background.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class background : MonoBehaviour
 {
     public GameObject go;
+    Vector3 centerPos;
     float distanceFromCamera = 12.5f;
     // Start is called before the first frame update
     void Start()
-    {
-        Vector3 centerPos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, distanceFromCamera));
-        go.transform.position = centerPos;
+    { 
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        centerPos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, distanceFromCamera));
+        go.transform.position = centerPos;
     }
 }
