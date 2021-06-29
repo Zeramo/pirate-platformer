cbf7686d254c393f3f8d9a9c6551f2101b9f10a3
Gabriel Budan <gabriel.budan@campus.lmu.de>
2021-06-29 10:15:40 +0200

    Added empty Script to rotate the grid and player can double jump (calculated with two raycasts)

--- Assets/Scripts/movementplayer.cs
+++ Assets/Scripts/movementplayer.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class movementplayer : MonoBehaviour
 {
-    public int playerSpeed = 10;
-    public int playerJumpPower = 1250;
+    public bool drawRaycasts;
 
-    public bool facingRight = true;
-    
+    [Header("Physics Forces")]
+    public int playerSpeed = 10;
+    public int playerJumpPower = 750;
+
     private float moveX;
+
+    [Header("Player Status")]
     public bool isGrounded;
+    public bool doubleJump;                 //Player has another jump available mid-air
+    public bool facingRight = true;
+
+    [Header("Collision Checks")]
+    public float coyote = .08f;
+    public float groundBuffer = .15f;
+    public float footOffset = .41f;         //Adjust, if Box Collider X size is changed
+    public LayerMask platformsLayer;
+
+    float playerHeight;
 
     BoxCollider2D boxCollider;
     Rigidbody2D rigidBody;
@@ -20,23 +33,52 @@ public class movementplayer : MonoBehaviour
         rigidBody = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
 
+        isGrounded = true;
+        doubleJump = false;
+
+        playerHeight = boxCollider.size.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        PhysicsCheck();
         PlayerMove();
+    }
+
+    void PhysicsCheck()
+    {
+        //Default value is false, we want to check every update cycle
+        isGrounded = false;
+
+        //Calculate Raycast for both feet, with footOffset and half player height as offset
+        RaycastHit2D leftLegHit = MyRaycast(new Vector2(-footOffset, -(playerHeight/2)), Vector2.down, groundBuffer, platformsLayer);
+        RaycastHit2D rightLegHit = MyRaycast(new Vector2(footOffset, -(playerHeight/2)), Vector2.down, groundBuffer, platformsLayer);
+        
+        //If one foot touches the ground, player is considered on the ground
+        if (leftLegHit || rightLegHit)
+        {
+            isGrounded = true;
+        }
+              
     }
 
     void PlayerMove(){
         //Controlls
         moveX = Input.GetAxis("Horizontal");
-        if(Input.GetButtonDown("Jump") ){ //&& isGrounded == true
+        if(Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            doubleJump = true;
+            Jump();
+        } else if (Input.GetButtonDown("Jump") && doubleJump)
+        {
+            doubleJump = false;
             Jump();
         }
+
         //Animation
         //Player direction
-        if(moveX < 0.0f && facingRight == true){
+        if (moveX < 0.0f && facingRight == true){
             FlipPlayer();
         }
         else if(moveX > 0.0f && facingRight ==false){
@@ -47,7 +89,9 @@ public class movementplayer : MonoBehaviour
     }
 
     void Jump(){
-        rigidBody.AddForce (Vector2.up * playerJumpPower);
+        rigidBody.bodyType = RigidbodyType2D.Static;
+        rigidBody.bodyType = RigidbodyType2D.Dynamic;
+        rigidBody.AddForce(Vector2.up * playerJumpPower);
     }
 
     void FlipPlayer(){ // replace once Animations come
@@ -65,5 +109,20 @@ public class movementplayer : MonoBehaviour
 
         }
         
+    }
+
+    RaycastHit2D MyRaycast(Vector2 offset, Vector2 direction, float length, LayerMask mask)
+    {
+        //Get position of player
+        Vector2 pos = transform.position;
+        //Calculate Raycast hit with offset from pos
+        RaycastHit2D hit = Physics2D.Raycast(pos + offset, direction, length, mask);
+
+        //REMOVE after game debugging is finished
+        Color color = hit ? Color.red : Color.green;
+        Debug.DrawRay(pos + offset, direction * length, color);
+        
+
+        return hit;
     }
 }
