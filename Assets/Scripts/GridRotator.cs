d134d04995fb6146bcf5d58970f80899bd68779e
Gabriel Budan <gabriel.budan@campus.lmu.de>
2021-06-29 16:01:47 +0200

    Ship will now rotate periodically

--- Assets/Scripts/GridRotator.cs
+++ Assets/Scripts/GridRotator.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class GridRotator : MonoBehaviour
 {
+    [Header("Properties of sinus function")]
+    public float amplitude = 5;
+    public float period = 2;
+
+    float time = 0f;                                //Time passed to sin function to calculate rotation, additive
+
+    float rotValue = 0f;
+    float prevRot = 0f;                             //To store previous rotation
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,21 @@ public class GridRotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Remove when a time limit is implemented
+        if (time > Mathf.PI/period * 999f)
+            time = 0f;
+        //Add time since last Update() to time
+        time += Time.deltaTime;
+        Debug.Log(time);
+        //Calculate rotation value as sin function
+        rotValue = amplitude * Mathf.Sin(period * time);
+
+        //Rotate by difference between current and previous value.
+        //Looking at a graph for visual representation is helpful for a better understanding.
+        transform.Rotate(0, 0, rotValue-prevRot);
+
+        //Save as previous rotation
+        prevRot = rotValue;
     }
+
 }
