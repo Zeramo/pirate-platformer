97db0f70e9e424e6c86413a9b4ee99e5c9ccedfa
Gabriel Budan <gabriel.budan@campus.lmu.de>
2021-07-16 21:12:09 +0200

    Player GameObject does not get destroyed anymore to avoid errors

--- Assets/Scripts/movementplayer.cs
+++ Assets/Scripts/movementplayer.cs
@@ -407,7 +407,6 @@ public class movementplayer : MonoBehaviour
             return;
         
         gameObject.SetActive(false);
-        DestroyPlayer();
         GameManager.PlayerDied();
     }
 
